a37f74416216a3a649a5eb3bc313a8216bf25a54
PNOML <vladtimspnoml@gmail.com>
2021-07-09 17:09:42 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,20 @@ import { Todo } from './models/Todo';
 import { User } from './models/User';
 
 function App() {
-    const [posts, setPosts] = useState<Post[] | []>([])
-    const [comments, setComments] = useState<Comment[] | []>([])
-    const [albums, setAlbums] = useState<Album[] | []>([])
-    const [photos, setPhotos] = useState<Photo[] | []>([])
-    const [todos, setTodos] = useState<Todo[] | []>([])
-    const [users, setUsers] = useState<User[] | []>([])
-    const [error, setError] = useState<string | null>(null)
+    const [posts, setPosts] = useState<Post[] | []>([]);
+    const [comments, setComments] = useState<Comment[] | []>([]);
+    const [albums, setAlbums] = useState<Album[] | []>([]);
+    const [photos, setPhotos] = useState<Photo[] | []>([]);
+    const [todos, setTodos] = useState<Todo[] | []>([]);
+    const [users, setUsers] = useState<User[] | []>([]);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     useEffect(
         () => {
             let a = new MyFirstClass();
 
+            setLoading(true);
             a.getApi()
                 .then((res) => {
                     setPosts(res.posts)
@@ -28,6 +30,7 @@ function App() {
                     setPhotos(res.photos)
                     setTodos(res.todos)
                     setUsers(res.users)
+                    setLoading(false);
                 })
                 .catch((e) => {
                     setError(e.message)
@@ -39,18 +42,24 @@ function App() {
     return (
         <div>
             <h1>JSONPlaceholders</h1>
-            {error ? (
-                <p>Произошла ошибка: {error}</p>
-            ) : (
-                <div>
-                    <div>Posts - {posts.length}</div>
-                    <div>Comments - {comments.length}</div>
-                    <div>Albums - {albums.length}</div>
-                    <div>Photos - {photos.length}</div>
-                    <div>Todos - {todos.length}</div>
-                    <div>Users - {users.length}</div>
-                </div>
+            {!loading && (
+                <>
+                    {error ? (
+                        <p>Произошла ошибка: {error}</p>
+                    ) : (
+                        <div>
+                            <div>Posts - {posts.length}</div>
+                            <div>Comments - {comments.length}</div>
+                            <div>Albums - {albums.length}</div>
+                            <div>Photos - {photos.length}</div>
+                            <div>Todos - {todos.length}</div>
+                            <div>Users - {users.length}</div>
+                        </div>
+                    )}
+                </>
             )}
+
+
         </div>
     )
 };
