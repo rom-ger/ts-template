b4d005e65d72019f01e9811235d75e4a56edbd9a
PNOML <vladtimspnoml@gmail.com>
2021-07-12 16:19:09 +0300

    fix mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ import { Todo } from './models/Todo';
 import { User } from './models/User';
 
 function App() {
-    const [posts, setPosts] = useState<Post[] | []>([]);
-    const [comments, setComments] = useState<Comment[] | []>([]);
-    const [albums, setAlbums] = useState<Album[] | []>([]);
-    const [photos, setPhotos] = useState<Photo[] | []>([]);
-    const [todos, setTodos] = useState<Todo[] | []>([]);
-    const [users, setUsers] = useState<User[] | []>([]);
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [comments, setComments] = useState<Comment[]>([]);
+    const [albums, setAlbums] = useState<Album[]>([]);
+    const [photos, setPhotos] = useState<Photo[]>([]);
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
 
@@ -24,17 +24,19 @@ function App() {
             setLoading(true);
             a.getApi()
                 .then((res) => {
-                    setPosts(res.posts)
-                    setComments(res.comments)
-                    setAlbums(res.albums)
-                    setPhotos(res.photos)
-                    setTodos(res.todos)
-                    setUsers(res.users)
-                    setLoading(false);
+                    setPosts(res.posts);
+                    setComments(res.comments);
+                    setAlbums(res.albums);
+                    setPhotos(res.photos);
+                    setTodos(res.todos);
+                    setUsers(res.users);
                 })
                 .catch((e) => {
-                    setError(e.message)
+                    setError(e.message);
                 })
+                .finally(() => { // непонятно почему не работает. я добавил в tsconfig es2018.promise в lib
+                    setLoading(false);
+                });
         },
         [],
     );
@@ -42,26 +44,22 @@ function App() {
     return (
         <div>
             <h1>JSONPlaceholders</h1>
-            {!loading && (
-                <>
-                    {error ? (
-                        <p>Произошла ошибка: {error}</p>
-                    ) : (
-                        <div>
-                            <div>Posts - {posts.length}</div>
-                            <div>Comments - {comments.length}</div>
-                            <div>Albums - {albums.length}</div>
-                            <div>Photos - {photos.length}</div>
-                            <div>Todos - {todos.length}</div>
-                            <div>Users - {users.length}</div>
-                        </div>
-                    )}
-                </>
+            {!loading && error && (
+                <p>Произошла ошибка: {error}</p>
             )}
 
-
+            {!loading && !error && (
+                <div>
+                    <div>Posts - {posts.length}</div>
+                    <div>Comments - {comments.length}</div>
+                    <div>Albums - {albums.length}</div>
+                    <div>Photos - {photos.length}</div>
+                    <div>Todos - {todos.length}</div>
+                    <div>Users - {users.length}</div>
+                </div>
+            )}
         </div>
-    )
+    );
 };
 
 export default App;
