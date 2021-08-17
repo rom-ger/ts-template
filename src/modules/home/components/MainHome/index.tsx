6127c03da9fc112549b4b902349abaed5537f1f6
PNOML <vladtimspnoml@gmail.com>
2021-08-17 20:52:09 +0300

    коммитим

--- src/modules/home/components/MainHome/index.tsx
+++ src/modules/home/components/MainHome/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const BUTTONS = ['HOME', 'PLOTS', 'APPS'];
 
 const MainHome = () => {
+    const [selectTabIndex, setSelectTabIndex] = useState<number>(0);
     return (
         <div>
-            MainHome
+            <div className="header-buttons">
+                {
+                    BUTTONS.map((name: string, index: number) =>
+                        <div
+                            className={`button-item ${selectTabIndex === index ? 'button-item--selected' : ''}`}
+                            onClick={() => setSelectTabIndex(index)}
+                        >{name}</div>
+                    )
+                }
+            </div>
+            <div className="header-content">
+                {
+                    selectTabIndex === 0 && <HomeButtons />
+                }
+                {
+                    selectTabIndex === 1 && <PlotsButtons />
+                }
+                {
+                    selectTabIndex === 2 && <AppsButtons />
+                }
+            </div>
         </div>
     );
 };
